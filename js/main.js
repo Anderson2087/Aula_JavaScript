5922d4b848761eb3f83c2d101edfed107235bf21
Anderson2087 <159357tu@gmail.com>
2022-05-30 20:25:31 -0300

    adicionado html com js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+var nome = "Anderson Fernando";
+var idade = 26;
+var frase = "Japão tem muitos Animes bons!!!";
+
+//alert serve para apresentar uma mensagem antes de iniciar a pagina.
+//alert("Meu Primeiro JS");
+//alert("Bem Vindo " + nome + " sua idade é " + idade);
+//Se depois da Variavel X for add um .replace é possibel alterar uma(s) palavra(s) palavra(s)
+//alert(frase.replace("Japão", "Brasil não"));
+
+//----------------------------------------------------------------------------------------------//
+
+//Auxilia a ver os resultados do codigo direto na pagina ao inspecionar a pagina na opção CONSOLE.
+console.log (nome);
+console.log (idade);
+//Se depois da Variavel X for add um .replace é possibel alterar uma(s) palavra(s) palavra(s)
+console.log (frase.replace("Japão", "Brasil não"));
+
