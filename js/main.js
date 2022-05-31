6a08cd6e193fb051f5fb3f7cce78b16de494721b
Anderson2087 <159357tu@gmail.com>
2022-05-31 18:09:07 -0300

    Mechendo com arreys e dicionario js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,37 @@
 
-var nome = "Anderson Fernando";
-var idade = 26;
-var frase = "Japão tem muitos Animes bons!!!";
+var frutas = [{nome: "Maça", cor:"Vermelha"}, {nome: "Uva", cor:"Roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+/*var fruta = {nome: "Maça", cor:"Vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);*/
+
+//----------------------------------------------------------------------------------------------//
+
+//var lista = ["Maça", "Pera", "Laranja"]
+
+//Acrecenta uma informação no final da lista/Arrey.
+//lista.push("Uva");
+//remove a ultima informação da lista/Arrey.
+//lista.pop();
+//O length serve para contar o numero de elementos do Arrey.
+//console.log(lista.length);
+
+//console.log(lista);
+
+//Remove a propiedade de Arrey e apresenta como string normal.
+//console.log(lista.toString());
+
+//Remove a propiedade de Arrey e apresenta como string normal alem de add um divisor de sua escolha.
+//console.log(lista.join(" - "));
+
+//----------------------------------------------------------------------------------------------//
+
+//var nome = "Anderson Fernando";
+//var idade = 26;
+//var frase = "Japão tem muitos Animes bons!!!";
 
 //alert serve para apresentar uma mensagem antes de iniciar a pagina.
 //alert("Meu Primeiro JS");
@@ -12,8 +42,8 @@ var frase = "Japão tem muitos Animes bons!!!";
 //----------------------------------------------------------------------------------------------//
 
 //Auxilia a ver os resultados do codigo direto na pagina ao inspecionar a pagina na opção CONSOLE.
-console.log (nome);
-console.log (idade);
+//console.log (nome);
+//console.log (idade);
 //Se depois da Variavel X for add um .replace é possibel alterar uma(s) palavra(s) palavra(s)
-console.log (frase.replace("Japão", "Brasil não"));
+//console.log (frase.replace("Japão", "Brasil não"));
 
